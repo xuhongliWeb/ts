6651c0538ba2331f956fee54561f43c971d167ce
xuhongliWeb <564507650@qq.com>
2020-07-31 09:27:51 +0800

    class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import './example/basic-type'
 // import './example/symbol'
 // import './example/interface'
-import './example/function'
+// import './example/function'
+import './example/es6-class2'
 
 const title: string = 'title'
